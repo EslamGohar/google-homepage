7ba49bc2cfe7491a2bb121935f6c7fe70bde0f11
eslamx <islamgoher1@gmail.com>
2021-09-05 20:59:40 +0200

    Google homepage UI

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container} from '../styles/center';
+
+
+
+const About = () => {
+    return (
+        <Container>
+            <h1>About</h1>   
+        </Container>
+    )
+}
+
+export default About
